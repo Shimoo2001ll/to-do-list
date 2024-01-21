5bc3485d06802ed9a391e9b92ef16e2c44602fa2
ALshimaa jamal <89547012+Shimoo2001ll@users.noreply.github.com>
2024-01-21 17:05:24 +0200

    news
    adding new style, some dom in js

--- main.js
+++ main.js
@@ -18,14 +18,19 @@ function addtasks(tasks) {
     tasks = taskin.value;
     if (tasks != '') {
         let div = document.createElement('div')
+        let container = document.createElement('div')
         let text = document.createTextNode(`${tasks}`)
         let btnDelet = document.createElement('button')
-        div.appendChild(text)
+        let textbtn = document.createTextNode(`delete`)
+        container.appendChild(text)
+        div.appendChild(container)
         div.appendChild(btnDelet)
+        btnDelet.appendChild(textbtn)
+        btnDelet.classList.add('delbtn')
         document.querySelector('.showTask').appendChild(div)
-        div.style.padding = '2%'
-        div.style.backgroundColor = 'white'
-        div.style.borderRadius = '7px'
+        div.classList.add('divTask')
+        container.classList.add('contain')
+
 
 
     }
