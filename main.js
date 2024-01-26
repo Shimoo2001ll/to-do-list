e55ec5687ca01f1947cdd5eb267c58d91cad7061
ALshimaa jamal <89547012+Shimoo2001ll@users.noreply.github.com>
2024-01-26 15:26:47 +0200

    new update
    adding functions to build the app

--- main.js
+++ main.js
@@ -1,53 +1,57 @@
 let taskin = document.querySelector(".task");
 let addbtn = document.querySelector(".btn");
-let alert = document.querySelector('.alert')
-addbtn.addEventListener('click', (e) => {
-        e = taskin.value;
-        if (e == '') {
-            alertfill()
-                // console.log('hi it is empty')
-        } else {
-            addtasks()
-            taskin.value = ''
+let alert = document.querySelector(".alert");
+addbtn.addEventListener("click", (e) => {
+    e = taskin.value;
+    if (e == "") {
+        alertfill();
+        // console.log('hi it is empty')
+    } else {
+        addtasks();
+        taskin.value = "";
+        localStorage.setItem('tasks', e)
 
-        }
-    })
-    //  add task in the body
+    }
+});
+//  add task in the body
 
 function addtasks(tasks) {
     tasks = taskin.value;
-    if (tasks != '') {
-        let div = document.createElement('div')
-        let container = document.createElement('div')
-        let text = document.createTextNode(`${tasks}`)
-        let btnDelet = document.createElement('button')
-        let textbtn = document.createTextNode(`delete`)
-        container.appendChild(text)
-        div.appendChild(container)
-        div.appendChild(btnDelet)
-        btnDelet.appendChild(textbtn)
-        btnDelet.classList.add('delbtn')
-        document.querySelector('.showTask').appendChild(div)
-        div.classList.add('divTask')
-        container.classList.add('contain')
-            //  delet btn  work
-        btnDelet.addEventListener('click', function() {
-            div.style.display = 'none'
-        })
-
-
+    if (tasks != "") {
+        let div = document.createElement("div");
+        let container = document.createElement("div");
+        let text = document.createTextNode(`${tasks}`);
+        let btnDelet = document.createElement("button");
+        let textbtn = document.createTextNode(`delete`);
+        container.appendChild(text);
+        div.appendChild(container);
+        div.appendChild(btnDelet);
+        btnDelet.appendChild(textbtn);
+        btnDelet.classList.add("delbtn");
+        document.querySelector(".showTask").appendChild(div);
+        div.classList.add("divTask");
+        container.classList.add("contain");
+        //  delet btn  work
+        btnDelet.addEventListener("click", function() {
+            div.style.display = "none";
+        });
     }
 }
 
 //show alert to fill the input filed
 
 function alertfill() {
-    if (taskin.value == '') {
-        alert.style.display = 'block';
-
+    if (taskin.value == "") {
+        alert.style.display = "block";
 
         setTimeout(() => {
-            alert.style.display = 'none';
+            alert.style.display = "none";
         }, 3000);
     }
+}
+
+//store data to local storage
+function storedata() {
+    e = taskin.value;
+    localStorage.setTimeout("Taskes", e);
 }
