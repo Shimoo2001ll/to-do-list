81a14366fc044fd14826bdea4d7656aade5f43d5
ALshimaa jamal <89547012+Shimoo2001ll@users.noreply.github.com>
2024-01-27 23:21:14 +0200

    new update
    adding new function to read and save data to local storage & show the data in the screen

--- main.js
+++ main.js
@@ -1,44 +1,93 @@
+// let taskin = document.querySelector(".task");
+// let addbtn = document.querySelector(".btn");
+// let alert = document.querySelector(".alert");
+// addbtn.addEventListener("click", (e) => {
+//     e = taskin.value;
+//     if (e == "") {
+//         alertfill();
+//         // console.log('hi it is empty')
+//     } else {
+//         addtasks();
+//         taskin.value = "";
+//         localStorage.setItem('tasks', e)
+
+//     }
+// });
+// //  add task in the body
+
+// function addtasks(tasks) {
+//     tasks = taskin.value;
+//     if (tasks != "") {
+//         let div = document.createElement("div");
+//         let container = document.createElement("div");
+//         let text = document.createTextNode(`${tasks}`);
+//         let btnDelet = document.createElement("button");
+//         let textbtn = document.createTextNode(`delete`);
+//         container.appendChild(text);
+//         div.appendChild(container);
+//         div.appendChild(btnDelet);
+//         btnDelet.appendChild(textbtn);
+//         btnDelet.classList.add("delbtn");
+//         document.querySelector(".showTask").appendChild(div);
+//         div.classList.add("divTask");
+//         container.classList.add("contain");
+//         //  delet btn  work
+//         btnDelet.addEventListener("click", function() {
+//             div.style.display = "none";
+//         });
+//     }
+// }
+
+// //show alert to fill the input filed
+
+// function alertfill() {
+//     if (taskin.value == "") {
+//         alert.style.display = "block";
+
+//         setTimeout(() => {
+//             alert.style.display = "none";
+//         }, 3000);
+//     }
+// }
+
+// //store data to local storage
+// function storedata() {
+//     e = taskin.value;
+//     localStorage.setTimeout("Taskes", e);
+// }
+// ------------------------------------------------------
 let taskin = document.querySelector(".task");
 let addbtn = document.querySelector(".btn");
 let alert = document.querySelector(".alert");
-addbtn.addEventListener("click", (e) => {
-    e = taskin.value;
-    if (e == "") {
-        alertfill();
-        // console.log('hi it is empty')
-    } else {
-        addtasks();
-        taskin.value = "";
-        localStorage.setItem('tasks', e)
+// create the arr of taskes
+let tasksarr;
+if (localStorage.Tskes !== "") {
+    tasksarr = JSON.parse(localStorage.Tskes)
 
-    }
-});
-//  add task in the body
-
-function addtasks(tasks) {
-    tasks = taskin.value;
-    if (tasks != "") {
-        let div = document.createElement("div");
-        let container = document.createElement("div");
-        let text = document.createTextNode(`${tasks}`);
-        let btnDelet = document.createElement("button");
-        let textbtn = document.createTextNode(`delete`);
-        container.appendChild(text);
-        div.appendChild(container);
-        div.appendChild(btnDelet);
-        btnDelet.appendChild(textbtn);
-        btnDelet.classList.add("delbtn");
-        document.querySelector(".showTask").appendChild(div);
-        div.classList.add("divTask");
-        container.classList.add("contain");
-        //  delet btn  work
-        btnDelet.addEventListener("click", function() {
-            div.style.display = "none";
-        });
-    }
+} else {
+    tasksarr = [];
 }
+// let tasksarr = [];
 
-//show alert to fill the input filed
+addbtn.onclick = function() {
+        if (taskin.value != '') {
+
+            let task = {
+                id: new Date,
+                title: taskin.value
+            }
+            tasksarr.push(task)
+                // savew to local storage
+            localStorage.setItem('Tskes', JSON.stringify(tasksarr))
+            showTasks()
+            taskin.value = ''
+        } else {
+            alertfill()
+
+        }
+
+    }
+    // //show alert to fill the input filed
 
 function alertfill() {
     if (taskin.value == "") {
@@ -49,9 +98,23 @@ function alertfill() {
         }, 3000);
     }
 }
+//  show data in the screen
 
-//store data to local storage
-function storedata() {
-    e = taskin.value;
-    localStorage.setTimeout("Taskes", e);
+function showTasks() {
+    let table = ''
+    for (let i = 0; i < tasksarr.length; i++) {
+        table += `   <tr>
+        <td>${tasksarr[i].id}</td>
+        <td>${tasksarr[i].title}</td>
+        <td>
+            <button class="btn delbtn">delete</button>
+        </td>
+    </tr>`
+
+    }
+    document.querySelector('.tableBody').innerHTML = table;
 }
+showTasks()
+btnDelet.addEventListener("click", function() {
+    div.style.display = "none";
+});
